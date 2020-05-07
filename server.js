7ba293ce7b73bebdcf50043b1339283a1102ec07
JoLeaper <joleap123@gmail.com>
2020-05-06 18:20:50 -0700

    added get functions and formatting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 // Application Dependencies
 const express = require('express');
 const cors = require('cors');
-
+const request = require('superagent');
 // Application Setup
 // - make an express app!
 const app = express();
@@ -12,13 +12,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // - enable CORS
 app.use(cors());
-const geoData = require('./data/geo.json');
-const weatherData = require('./data/weather.json');
 
-app.get('/location', (req, res) => {
+app.get('/location', async(req, res) => {
     try {
-        const object = formatObject();
-        res.json(object);
+        const searchedLocation = getLocation(res);
+        res.json(searchedLocation);
     }
     catch(err) {
         res.status(500).send('Sorry something went wrong');
@@ -26,37 +24,62 @@ app.get('/location', (req, res) => {
 });
 app.get('/weather', (req, res) => {
     const object = formatWeatherObject();
-    console.log(object);
     res.json(object);
     
 });
 
 app.listen(PORT, () => console.log('listening on 3001'));
 
-function formatObject(res) {
-    if(res !== 'Portland') {
+// gets the object
+const getLocation = async(res) => {
+    const geoData = await request.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.GEOCODE_API_KEY}&q=${res}&format=json`);
+    const parsedData = JSON.parse(geoData.body)[0];
+    return parsedData;
+};
+
+// formats the object
+function formatObject(firstObject) {
+    try {
+        const firstFormattedObject = {
+            formatted_query: firstObject.display_name,
+            latitude: firstObject.lat,
+            longitude: firstObject.lon,
+        };
+        return firstFormattedObject;
+    }
+    catch(err) {
         throw new Error();
     }
-    const firstObject = geoData[0];
-    const queryNameArray = firstObject.display_name.split(' ');
+}
 
-    const firstFormattedObject = {
-        formatted_query: queryNameArray[0] + ' ' + queryNameArray[3] + ' ' + queryNameArray[4],
-        latitude: firstObject.lat,
-        longitude: firstObject.lon,
-    };
-    return firstFormattedObject;
+function getLatLon(firstObject) {
+    try {
+        const latLonObject = {
+            latitude: firstObject.lat,
+            longitude: firstObject.lon,
+        };
+        return latLonObject;
+    }
+    catch(err) {
+        throw new Error();
+    }
 }
 
 function formatWeatherObject() {
     const weatherArray = weatherData.data;
+    try {
+        const formattedObjectArray = weatherArray.map(weatherObject => {
+            return {
+                forecast: weatherObject.weather.description,
+                time: weatherObject.datetime
+            };
+        });
+        return formattedObjectArray;
+    
+    }
+    catch(err) {
+        throw new Error();
+    }
 
-    const formattedObjectArray = weatherArray.map(weatherObject => {
-        return {
-            forecast: weatherObject.weather.description,
-            time: weatherObject.datetime
-        };
-    });
-    return formattedObjectArray;
 
 }
